b2c3a41f18d48b755f0c91989a6f1c5049efb882
maheshesh <soudisha@gmail.com>
2019-11-11 21:52:44 -0600

    Adding kubernets sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,23 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=maheshe/mahadocker1:latest
+Username=maheshe
+password=welcome123
+echo "Docker ID and Image Authenticate Now: $dockerpath"
+docker login --username $Username --password $password
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --generator=run-pod/v1 mahadocker1 --image=maheshe/mahadocker1:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+echo "sleep for 12 seconds while waiting for pod"
+sleep 12
+kubectl port-forward deployment/mahadocker1 8080:80 
+kubectl logs --selector app=mahadocker1
